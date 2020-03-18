9c40ff8e7407c826551abb76050bbd998abc5219
Ralf Fruehwald <rfruehwald@vmware.com>
2020-03-18 13:38:00 +0100

    removed

--- hello/test.py
+++ hello/test.py
@@ -1,17 +1,9 @@
 import requests
 
 
-print (sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    test = "Test"
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
 r = requests.get("http://www.flieg-maxi-flieg.de")
 print(r.status_code)
+print(r.ok)
+
 # print (greet("World"))
 # print (greet("Ralf"))
